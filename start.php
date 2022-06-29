479a2c54ef0b1896d14fe5ae316ccda23a300f00
Josh Garcia <joshgarcia7410@gmail.com>
2022-06-29 14:41:19 -0700

    adds functions

--- start.php
+++ start.php
@@ -18,6 +18,27 @@
     for($i = 0; $i <= 10; $i++) {
         echo $i;
     }
+
+    $numbers = [1,5,8,3];
+    foreach($numbers as $number){
+        echo $number;
+    }
+
+    function hero_Section(){
+        $heading_hero = ["heading" => "<h1>Dog Training</h1>", "description" => "<p>We do dog training on the daily</p>"];
+
+        echo $heading_hero["heading"] . " " . $heading_hero["description"];
+    }
+
+    function about_Section() {
+        $heading_about = ["about" => "<h4>What we do</h4>", "heading-2" => "<h2>About Us</h2>", "description-2" => "<p>This is an intro. This is what we do.</p>"];
+        echo $heading_about["about"] . " " . $heading_about["heading-2"] . " " . $heading_about["description-2"];
+    }
+
+    function pull_Together() {
+        hero_Section();
+        about_Section();
+    }
     ?>
 </body>
 </html>
