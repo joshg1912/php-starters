6552add4f2f57376a94c17aedfa6fea991359562
Josh Garcia <joshgarcia7410@gmail.com>
2022-06-29 10:13:17 -0700

    adds associative array

---
+++ start.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $data = ["site_title" => "<h1>Dog Training</h1>", "site_desc" => "<p>We train dogs on the daily</p>"];
+
+    echo $data["site_tite"] . " " . $data["site_desc"];
+    
+    ?>
+</body>
+</html>
